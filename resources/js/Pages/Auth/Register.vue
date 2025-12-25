34cb6831b3a4ec8f1888dfc570f28106fa26c51f
Yohan Seneret <yohan.seneret@gmail.com>
2025-12-26 00:18:56 +0100

    inertia form register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -6,36 +6,53 @@ import Layout from '../../Layouts/Layout.vue';
 
 <script setup>
 import { reactive } from 'vue';
+// import { router} from '@inertiajs/vue3';
+ import {useForm} from '@inertiajs/vue3';
+import { route } from '../../../../vendor/tightenco/ziggy/src/js';
 
-    const form = reactive({
+
+    // const form = reactive({
+    const form = useForm({
         name: null,
         email: null,
-        password:null,
+        password: null,
         password_confirmation: null
     })
 
     const submit = () => {
-        console.log(form);
-    }
+        // router.post('/register', form)
+        form.post(route('register'),
+            {
+                onError:() => form.reset('password', password_confirmation),
+                preserveScroll: true
+            }
+        )
+    };
 </script>
 <template>
     <Head :title="`${$page.component}`"/>
     <h1 class="title">Register a new account </h1>
 
     <div class="w-2/4 mx-auto">
-        <!-- Pas besoin d'action avec le form car il se gère avec inertiajs -->
+        <!-- Pas besoin d'action avec le form car il se gère avec inertiajs 
+        Le CRSF Token se fait automatiquement avec inertia-->
         <form @submit.prevent="submit">
             <div class="mb-6">
                 <label for="name">Name</label>
                 <input type="text" v-model="form.name">
+                <small> {{ form.errors.name }}</small>
             </div>
             <div class="mb-6">
                 <label for="name">Email</label>
                 <input type="text" v-model="form.email">
+                <small> {{ form.errors.email}}</small>
+
             </div>
             <div class="mb-6">
                 <label for="name">Password</label>
                 <input type="password" v-model="form.password">
+                <small> {{ form.errors.password }}</small>
+
             </div>
             <div class="mb-6">
                 <label for="name">Confirm Password</label>
@@ -43,7 +60,7 @@ import { reactive } from 'vue';
             </div>
             <div>
                 <p class="text-slate-600 mb-2"> Already a user? <a href="#" class="text-link">Login</a></p>
-                <button class="primary-btn">Register</button>
+                <button class="primary-btn" :disabled="form.processing">Register</button>
             </div>
         </form>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::inertia('/', 'Home')->name('home');
 
 Route::inertia('/register', 'Auth/Register')->name('register');
+
+
+Route::post('/register', [AuthController::class, 'register']);
 
 /* Route::get('/about', function () {
     return inertia('About', ['user' => 'Mike']);
