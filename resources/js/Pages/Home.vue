18d396b63aa2b46c04e712675a20b5d27075a518
Yohan Seneret <yohan.seneret@gmail.com>
2025-12-25 18:43:33 +0100

    p3 Layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,8 @@
+<script setup>
+    import Layout from '../Layouts/Layout.vue';
+    defineOptions({ layout: Layout})
+</script>
+
 <template>
-    <div>
         <h1 class="text-3xl font-bold underline">Hello</h1>
-    </div>
 </template>
