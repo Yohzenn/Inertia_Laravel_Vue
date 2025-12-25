a6df04bccdbe0f0a7db9433bef4d485787c94206
Yohan Seneret <yohan.seneret@gmail.com>
2025-12-25 21:42:44 +0100

    feat: install ziggy

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
-});
+})->name('home');
 
 /* Route::get('/about', function () {
     return inertia('About', ['user' => 'Mike']);
 });  Pareil en dessous */
 
-Route::inertia('/about', 'About', [ 'user' => 'Mike']);
+Route::inertia('/about', 'About', [ 'user' => 'Mike'])->name('about');
