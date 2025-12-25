04b0b6147d00c111257241fb0e83aded05e531c6
Yohan Seneret <yohan.seneret@gmail.com>
2025-12-25 17:43:59 +0100

    p2 routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('Home');
 });
+
+/* Route::get('/about', function () {
+    return inertia('About', ['user' => 'Mike']);
+});  Pareil en dessous */
+
+Route::inertia('/about', 'About', [ 'user' => 'Mike']);
