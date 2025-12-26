5ae0269b914a0c84e9520e4babb773f921b079ec
Yohan Seneret <yohan.seneret@gmail.com>
2025-12-27 00:45:48 +0100

    Pagination

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @routes
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     @inertiaHead
-    @routes
 </head>
 <body>
     @inertia
