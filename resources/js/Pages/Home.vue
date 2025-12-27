0229d3b22fa4bce0fa2560fdfa79904dd8e983bc
Yohan Seneret <yohan.seneret@gmail.com>
2025-12-27 12:40:57 +0100

    exemple of policy

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,6 +7,7 @@ import { debounce } from 'lodash';
 const props = defineProps({
     users: Object,
     searchTerm: String,
+    can: Object
 }
 );
 // Format date
@@ -41,6 +42,7 @@ const getDate = (date) =>
                     <th>Name</th>
                     <th>Email</th>
                     <th>Registration Date</th>
+                    <th v-if="can.delete_user">Delete</th>
                 </tr>
             </thead>
             
@@ -55,6 +57,9 @@ const getDate = (date) =>
                     <td>{{ user.name }}</td>
                     <td>{{ user.email }}</td>
                     <td>{{ getDate(user.created_at) }}</td>
+                    <td v-if="can.delete_user"> 
+                        <button class="bg-red-500 w-6 h-6 rounded-full"></button>
+                    </td>
                 </tr>
             </tbody>
         </table>
